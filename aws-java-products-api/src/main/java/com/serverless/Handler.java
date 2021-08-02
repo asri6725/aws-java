0231c0088fb19a6f3763f0e4044df81fa8d1bc62
abhinandan.srinivas <a.d.srinivas@accenture.com>
2021-08-02 15:52:25 +1000

    Changes

--- aws-java-products-api/src/main/java/com/serverless/Handler.java
+++ aws-java-products-api/src/main/java/com/serverless/Handler.java
@@ -2,21 +2,29 @@ package com.serverless;
 
 import java.util.Collections;
 import java.util.Map;
-
+import java.util.UUID;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private static final Logger LOG = LogManager.getLogger(Handler.class);
-
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: {}", input);
+		System.out.println(input)
+		AmazonDynamoDB dynamoDb = AmazonDynamoDBClientBuilder.standard().build();
+		System.out.println('Built a dynamo connector')
 		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+		
+		String keyName = UUID.randomUUID().toString();
+		
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
