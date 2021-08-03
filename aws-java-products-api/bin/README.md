c7706a8e29ea256519912143c18a5c56998627d6
asri6725 <abhinandan.srinivas@gmail.com>
2021-08-04 01:04:40 +1000

    Working!

---
+++ aws-java-products-api/bin/README.md
@@ -0,0 +1,9 @@
+# Introduction 
+Java App with behaviour similar to Python app with one lambda post event handler, still has little testing to finish up
+
+# Build and Test
+Frontend build is the same
+{Not included yet}
+Java Buil
+1. mvn clean install
+2. sls deploy --account_id {account_id}
--- aws-java-products-api/src/main/java/com/serverless/Handler.java
+++ aws-java-products-api/src/main/java/com/serverless/Handler.java
@@ -2,9 +2,14 @@ package com.serverless;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.UUID;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.*;
+import java.io.*;
+import org.json.*;
+import org.json.simple.parser.*;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -14,21 +19,45 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+public class Handler implements RequestHandler<Map<String,Object>, ApiGatewayResponse> {
 
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		System.out.println(input)
-		AmazonDynamoDB dynamoDb = AmazonDynamoDBClientBuilder.standard().build();
-		System.out.println('Built a dynamo connector')
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+	public ApiGatewayResponse handleRequest(Map<String,Object> input, Context context) {
+		int statusCode = 200;
+		Response responseBody = new Response("Success");
+		try {
+			JSONObject event = new JSONObject((String)input.get("body")) ;
+			AmazonDynamoDB dynamoDb = AmazonDynamoDBClientBuilder.standard().build();
+			String keyName = UUID.randomUUID().toString();
+			String tableName = "dummy-details";
+			
+			Map<String, AttributeValue> attributes = new HashMap<String, AttributeValue>();
+			attributes.put("uid",  new AttributeValue().withS(keyName));
+			attributes.put("firstname",  new AttributeValue().withS(event.getString("firstname")));
+			attributes.put("lastname",  new AttributeValue().withS(event.getString("lastname")));
+			attributes.put("secret",  new AttributeValue().withS(event.getString("secret")));
+			dynamoDb.putItem(new PutItemRequest()
+							.withTableName(tableName)
+							.withItem(attributes));
+		} catch (Exception e) {
+			statusCode = 500;
+			System.out.println(e);
+		}
 		
-		String keyName = UUID.randomUUID().toString();
-		
+		Map<String, String> headers = new HashMap<>();
+		headers.put( "Content-Type", "application/json");
+		headers.put( "Access-Control-Allow-Origin", "*");
+		headers.put( "Cache-Control", "no-store");
+		headers.put( "X-Powered-By", "AWS Lambda & serverless");
 		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
+				.setStatusCode(statusCode)
 				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.setHeaders(headers)
 				.build();
 	}
+	private Map<String, AttributeValue> convert(Map<String, String> map) {
+        return Optional.ofNullable(map).orElseGet(HashMap::new).entrySet().stream().collect(
+            Collectors.toMap(Map.Entry::getKey, e -> new AttributeValue().withS(e.getValue())));
+    }
 }
